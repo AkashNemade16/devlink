b6fd0a579b6320f0687b858cafd6f75b93cd7767
akash <aksh.akash@gmail.com>
2023-10-03 10:34:01 -0500

    devLinks

--- app/(dashboard)/Homepage/page.tsx
+++ app/(dashboard)/Homepage/page.tsx
@@ -5,6 +5,11 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div className='w-full'>
+      <div className='flex flex-col mt-4 mb-4'>
+      <h1 className='flex text-[24px] font-[700] '>Customize your links</h1>
+      <p className='text-grey text-[16px] font-[400]'>Add/edit/remove links below and then share all your profiles with the world!</p>
+      </div>
+      <div>
       <Button
         text='+ Add new Link'
         textColor='text-purple'
@@ -12,6 +17,7 @@ const Homepage = () => {
         color='bg-white'
         disabled={false}
       />
+      </div>
     </div>
   )
 }
