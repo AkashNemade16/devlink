dee8e4e005bf8ef303abd95e142a2bff4fed3d17
akash <aksh.akash@gmail.com>
2023-10-12 23:05:41 -0500

    footer save

--- app/(dashboard)/dashFooter.tsx
+++ app/(dashboard)/dashFooter.tsx
@@ -4,7 +4,7 @@ import Button from "../components/Button";
 import { useGlobalContext } from "./(context)/store";
 const DashFooter = () => {
   const {title,url,userId,showLinks,setShowLinks,links} = useGlobalContext()
-console.log(links)
+console.log('footer',links,url,title)
   return (
     <div className="w-full flex flex-col border-t-2 border-greyShade mt-2 mb-2">
       <div className="mt-3 border-lightPurple">
@@ -13,9 +13,9 @@ console.log(links)
           onClick={() => {
             console.log("clicked");
           }}
-          color={`${showLinks?"bg-purple":"bg-purple opacity-50"}`}
+          color={`${links.length===0?"bg-purple opacity-50":"bg-purple"}`}
           textColor="text-white"
-          disabled={!showLinks}
+          disabled={links.length===0?true:false}
         />
       </div>
     </div>
