067e8d35b5b2c3e21b368b8ce4801380e43727d9
akash <aksh.akash@gmail.com>
2023-10-25 17:09:13 -0500

    fixed frontend mentor styling

--- app/components/PreviewButton.tsx
+++ app/components/PreviewButton.tsx
@@ -24,16 +24,18 @@ const PreviewButton = ({ type, input, imageUrl, color }: buttonProps) => {
     twitchColor: "bg-twitchColor",
     freeCodeCampColor: "bg-freeCodeCampColor",
   }
+  
   return (
-    <div className={`flex w-[237px] flex-col rounded-md justify-center items-center ${colorVariants[color as keyof typeof colorVariants]}  mt-3`}>
+    <div className={`flex w-[237px] h-[56px] flex-col rounded-md justify-center items-center ${colorVariants[color as keyof typeof colorVariants]}  mt-3`}>
       <Link className="w-full pr-3 pl-3" href={input || ""}>
         <div className="flex flex-row justify-between w-full">
           <div className="flex">
             {<Image src={imageUrl} height={20} width={20} alt={type} />}
           </div>
-          <div className="flex text-white">{type}</div>
+          <div className={`flex ${type==='FrontendMentor'?'text-black':'text-white'}`}>{type}</div>
           <div className="flex">
             <Image
+              style={{filter: type==='FrontendMentor'?'invert(100%)':'invert(0%)'}}
               src={"images/icon-arrow-right.svg"}
               height={20}
               width={20}
