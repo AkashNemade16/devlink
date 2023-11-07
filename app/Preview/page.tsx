784fda01577131eaa8c5d7f2a0d6453ca9e93fb8
akash <aksh.akash@gmail.com>
2023-11-07 00:53:13 -0600

    stylings fixed

--- app/Preview/page.tsx
+++ app/Preview/page.tsx
@@ -1,38 +1,28 @@
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
+
 const Preview = () => {
   return (
-    <div className='flex flex-col w-full h-[357px] bg-purple rounded-l-2xl rounded-r-2xl'>
-      <div className='flex h-[30px] bg-purple'>
+    <div className='sm:hidden md:flex flex-col w-full md:h-[357px] md:bg-purple md:rounded-l-2xl md:rounded-r-2xl'>
+      <div className='hidden md:flex md:h-[30px] md:bg-purple'>
       </div>
       <div className='flex w-full bg-white h-[70px]'>
         <div className='flex flex-row w-full justify-between mt-2 mb-2'>
-        <div className="flex items-center border-2 border-purple rounded-md p-3">
+        <div className="flex w-full items-center justify-center border-2 border-purple rounded-md p-3 mr-1">
                 <Link href={'/Preview'}>
-                    <Image
-                        className="md:hidden m-1"
-                        src={"images/icon-preview-header.svg"}
-                        height={30}
-                        width={30}
-                        alt="icon"
-                    />
-                    <div className="hidden md:flex text-purple">Back to Editor</div>
+                    <div className="flex text-purple">Back to Editor</div>
                 </Link>
             </div>
-            <div className="flex items-center border-2 border-purple bg-purple rounded-md p-1">
+            <div className="flex items-center justify-center border-2 border-purple bg-purple rounded-md p-3 w-full ml-1">
                 <Link href={'/Preview'} className="flex">
-                    <Image
-                        className="md:hidden m-1"
-                        src={"images/icon-preview-header.svg"}
-                        height={30}
-                        width={30}
-                        alt="icon"
-                    />
                     <div className="text-white">Share Link</div>
                 </Link>
             </div>
         </div>
+      </div>
+      <div className='flex'>
+        
+
       </div>
     </div>
   )
