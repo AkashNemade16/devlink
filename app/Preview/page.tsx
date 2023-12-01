2dd194006dfbc4dc4e04bd227dd6750d09e22d40
akash <aksh.akash@gmail.com>
2023-12-01 12:05:52 -0600

    fixed preview page stylings

--- app/Preview/page.tsx
+++ app/Preview/page.tsx
@@ -50,23 +50,22 @@ const Preview = () => {
   return (
     <div className='flex w-full max-h-screen'>
       <div className='sm:hidden md:flex flex-col w-full md:h-[357px] md:bg-purple md:rounded-l-2xl md:rounded-r-2xl'>
-      <div className='hidden md:flex md:h-[30px] md:bg-purple'>
-      </div>
-      <div className='flex w-full bg-white h-[70px]'>
+      
+      <div className='flex w-full flex-col items-center justify-center'>
+      <div className='relative flex w-[95%] top-6 bg-white rounded-xl'>
         <div className='flex flex-row w-full justify-between mt-2 mb-2'>
-        <div className="flex w-full items-center justify-center border-2 border-purple rounded-md p-3 mr-1">
+        <div className="flex items-center justify-center border-2 border-purple rounded-md p-3 ml-2">
                 <Link href={'/Profile'}>
                     <div className="flex text-purple">Back to Editor</div>
                 </Link>
             </div>
-            <div className="flex items-center justify-center border-2 border-purple bg-purple rounded-md p-3 w-full ml-1">
-                <Link href={'/'} className="flex">
+            <div className="flex items-center justify-center border-2 border-purple bg-purple rounded-md p-3 mr-2">
+                <Link href={'/'}>
                     <div className="text-white">Share Link</div>
                 </Link>
             </div>
         </div>
       </div>
-      <div className='flex w-full justify-center'>
       <div className='flex mt-[100px] justify-center rounded-2xl bg-white w-[349px]'>
           <div className='flex flex-col items-center justify-center h-full p-3'>
             <div className='flex items-center rounded-full overflow-hidden w-[96px] h-[96px] justify-center'>
